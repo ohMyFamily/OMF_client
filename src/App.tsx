bac441070a4e21a200fe2536a719cc4216775f9c
skrgus6269 <knh6269@gmail.com>
2024-11-08 11:28:06 +0900

    OMF-20 feat: Appbar 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -7,29 +7,35 @@ import {
   Title1,
   Title2,
   Title3,
-} from './components/common/Typography';
+} from '@components/common/Typography';
+import AppBar from '@components/common/AppBar';
+import $ from './App.module.scss';
+import classNames from 'classnames';
 
 function App() {
   return (
     <>
-      <Title1>The quick brown fox jumps over the lazy dog. </Title1>
-      <Title2>The quick brown fox jumps over the lazy dog.</Title2>
-      <Title3>The quick brown fox jumps over the lazy dog.</Title3>
-      <Button1>
-        The quick brown fox jumps over the lazy dog.
-        <br />
-      </Button1>
-      <Button2>
-        The quick brown fox jumps over the lazy dog.
-        <br />
-      </Button2>
-      <Button2 underline={true}>
-        The quick brown fox jumps over the lazy dog.
-        <br />
-      </Button2>
-      <Body1>The quick brown fox jumps over the lazy dog.</Body1>
-      <Body2> The quick brown fox jumps over the lazy dog.</Body2>
-      <Caption1> The quick brown fox jumps over the lazy dog.</Caption1>
+      <div className={$.Wrapper}>
+        <AppBar leftRole='back' rightRole='close' className={classNames($.appbar)} />
+        <Title1>The quick brown fox jumps over the lazy dog. </Title1>
+        <Title2>The quick brown fox jumps over the lazy dog.</Title2>
+        <Title3>The quick brown fox jumps over the lazy dog.</Title3>
+        <Button1>
+          The quick brown fox jumps over the lazy dog.
+          <br />
+        </Button1>
+        <Button2>
+          The quick brown fox jumps over the lazy dog.
+          <br />
+        </Button2>
+        <Button2 underline={true}>
+          The quick brown fox jumps over the lazy dog.
+          <br />
+        </Button2>
+        <Body1>The quick brown fox jumps over the lazy dog.</Body1>
+        <Body2> The quick brown fox jumps over the lazy dog.</Body2>
+        <Caption1> The quick brown fox jumps over the lazy dog.</Caption1>
+      </div>
     </>
   );
 }

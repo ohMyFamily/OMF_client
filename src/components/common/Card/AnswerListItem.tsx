ed2ef8453dceff20ec15e4cac621816fc4665f5f
skrgus6269 <knh6269@gmail.com>
2024-11-12 21:31:54 +0900

    OMF-16 refactor: AnswerListItem 컴포넌트 최적화

--- src/components/common/Card/AnswerListItem.tsx
+++ src/components/common/Card/AnswerListItem.tsx
@@ -1,50 +1,41 @@
-import { useState } from 'react';
+import { Dispatch, SetStateAction } from 'react';
 import classNames from 'classnames';
 import $ from './answerlistitem.module.scss';
-import Body2 from '../Typography/Body2';
+import { Body2, Button1 } from '../Typography';
 
 interface AnswerListItemProps {
-    nickname: string;
-    score: number;
-    src: string;
+  nickname: string;
+  score: number;
+  cardImage: string;
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
 }
 
-function AnswerListItem({ nickname, score, src }: AnswerListItemProps) {
-    const [isOpen, setIsOpen] = useState(false);
+function AnswerListItem({ nickname, score, cardImage, isOpen, setIsOpen }: AnswerListItemProps) {
+  const handleClick = async () => {
+    if (!isOpen) {
+      setIsOpen(true);
+    }
+  };
 
-    const handleClick = () => {
-        if (!isOpen) {
-        setIsOpen(true);
-        }
-    };
-
-    return (
-        <div 
-        className={classNames($.answerContainer, {
-            [$.openState]: isOpen
-        })}
-        onClick={handleClick}
-        >
-        <img src={src} alt={`${nickname}의 프로필`} />
-        <div className={classNames($.description)}>
-            {isOpen ? (
-            <>
-                <span>{score}점!</span>
-                <div className={classNames($.message)}>
-                <Body2>{nickname}님이 채점한 답안지</Body2>
-                </div>
-            </>
-            ) : (
-            <>
-                <span>{nickname}님이</span>
-                <div className={classNames($.message)}>
-                <Body2>채점한 답안지가 도착했어요!</Body2>
-                </div>
-            </>
-            )}
+  return (
+    <div
+      className={classNames($.answerContainer, {
+        [$.openState]: isOpen,
+      })}
+      onClick={handleClick}
+    >
+      <img src={cardImage} alt={`${nickname}의 프로필`} />
+      <div className={classNames($.description)}>
+        <span>
+          <Button1>{isOpen ? `${score}점!` : nickname}</Button1>
+        </span>
+        <div className={classNames($.message)}>
+          <Body2>{isOpen ? `${nickname}님이 채점한 답안지` : `채점한 답안지가 도착했어요!`}</Body2>
         </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default AnswerListItem;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,20 +3,36 @@ import $ from '../App.module.scss';
 import ResultCard from '@/components/common/Card/ResultCard';
 import GradingCard from '@/components/common/Card/GradingCard';
 import AnswerListItem from '@/components/common/Card/AnswerListItem';
-
+import { useEffect, useRef, useState } from 'react';
+import Call from '@/assets/svg/Tossface/Call.svg';
+import AnimateCardSample from '@/components/domain/CheckScore/AnimateCardSample';
 
 const IndexPage = () => {
   const navigate = useNavigate();
 
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
-    <div className={$.Wrapper}>
-      <button onClick={() => navigate('/homepage')}>이동</button>
-      <ResultCard score={100} />
-      <GradingCard title="title" src={''} number={0} answer={''}/>
-      <AnswerListItem />
-    </div>
-      </>
+      <div className={$.Wrapper}>
+        <button onClick={() => navigate('/homepage')}>이동</button>
+        <ResultCard score={100} variant='score' />
+        <AnimateCardSample />
+        <GradingCard
+          title={`어머니와\n언제 마지막으로 전화했나요?`}
+          cardImage={Call}
+          cardNumber={1}
+          answer={'정답'}
+        />
+        <AnswerListItem
+          nickname='낙현'
+          score={80}
+          cardImage=''
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+        />
+      </div>
+    </>
   );
 };
 

28daa3d065013fef1c4accdf098d45ac1dce29b4
skrgus6269 <knh6269@gmail.com>
2024-11-12 21:30:42 +0900

    OMF-16 refactor: ScoreButton 컴포넌트 리팩토링

--- src/components/common/Card/GradingCard.tsx
+++ src/components/common/Card/GradingCard.tsx
@@ -1,46 +1,39 @@
 import classNames from 'classnames';
 import $ from './gradingcard.module.scss';
-import ScoreButton from "../Item/ScoreButton";
-import Title3 from "../Typography/Title3"
+import ScoreButton from '../Item/ScoreButton';
+import Title3 from '../Typography/Title3';
 import Caption1 from '../Typography/Caption1';
 import Body1 from '../Typography/Body1';
-import Call from '../../../assets/svg/Tossface/Call.svg';
+import { TOTAL_QUESTION } from '@/constants/business.constants';
+import { useState } from 'react';
 
 interface GradingCardProps {
-    title: string;
-    src: string;
-    number: number;
-    answer: string;
+  title: string;
+  cardImage: string;
+  cardNumber: number;
+  answer: string;
 }
-function GradingCard({number, title, answer, src}: GradingCardProps) {
-
-    return (
-        <div className={classNames($.gradingcard)}>
-
-            <div className={classNames($.number)}>
-                <Caption1>{number}/10</Caption1>
-            </div>
-
-            <div className={classNames($.questionWrapper)}> 
-                <img src={Call}/>
-                {/* <img className={classNames($.questionImg)} src={src}/> */}
-
-                <Title3>
-                    {/* {title} */}
-                    어머니와<br/>
-                    언제 마지막으로 통화했나요?
-                </Title3>
-            </div>
-
-            <div className={classNames($.answerWrapper)}>
-                <Body1>answer{answer}</Body1>
-            </div>
-
-            <div className={classNames($.ScoreButton)}>
-            <ScoreButton items={['correct', 'incorrect']}/>
-            </div>
-        </div>
-    )
+function GradingCard({ cardNumber, title, answer, cardImage }: GradingCardProps) {
+  const [correctState, setCorrectState] = useState<boolean | null>(null);
+  return (
+    <div className={classNames($.gradingcard)}>
+      <div className={classNames($.number)}>
+        <Caption1>
+          {cardNumber}/{TOTAL_QUESTION}
+        </Caption1>
+      </div>
+      <div className={classNames($.questionWrapper)}>
+        <img src={cardImage} />
+        <Title3>{title}</Title3>
+      </div>
+      <div className={classNames($.answerWrapper)}>
+        <Body1>{answer}</Body1>
+      </div>
+      <div className={classNames($.ScoreButton)}>
+        <ScoreButton state={correctState} setState={setCorrectState} />
+      </div>
+    </div>
+  );
 }
 
-export default GradingCard
+export default GradingCard;
--- src/components/common/Item/ScoreButton.tsx
+++ src/components/common/Item/ScoreButton.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Dispatch, SetStateAction, useEffect } from 'react';
 import classNames from 'classnames';
 import $ from './scorebutton.module.scss';
 import Button1 from '../Typography/Button1';
@@ -6,55 +6,49 @@ import XCircle from '@/assets/svg/XCircle.svg?react';
 import CheckCircle from '@/assets/svg/CheckCircle.svg?react';
 
 interface ScoreButtonProps {
-    items: ('correct' | 'incorrect')[];
+  state: boolean | null;
+  setState: Dispatch<SetStateAction<boolean | null>>;
 }
 
+const ScoreButton = ({ state, setState }: ScoreButtonProps) => {
+  const handleClick = (state: boolean) => {
+    setState(state);
+  };
 
-const ScoreButton = ({ items }: ScoreButtonProps) => {
-    // 초기값: 아무것도 선택하지 않은 상태 -1
-    const [selectedIndex, setSelectedIndex] = useState<number>(-1);
-
-    const handleClick = (index: number) => {
-        if (selectedIndex === -1 || selectedIndex !== index) {
-            setSelectedIndex(index);
-            console.log('선택 항목:', index);
-        }
-    };
-    return (
-        <div className={classNames($.scoreButtonContainer)}>
-            {items.map((type, index) => (
-                <div 
-                    key={index} 
-                    className={classNames($.scoreButtonItem, $[`scoreButtonItem${type}`], {
-                        [$[`scoreButtonItemClicked`]]: selectedIndex === index
-                    })}
-                    onClick={() => handleClick(index)}
-                >
-                    {type === 'correct' ? (
-                        <CheckCircle 
-                            className={classNames($.icon, {
-                                [$.iconCorrectClicked]: selectedIndex === index,
-                            })}
-                        />
-
-                    ) : (
-                        <XCircle 
-                            className={classNames($.icon, {
-                                [$.iconIncorrectClicked]: selectedIndex === index,
-                            })}
-                        />
-                    )}
-                    <div className={classNames($.textWrapper, {
-                        [$.textClicked]: selectedIndex === index
-                    })}>
-                        <Button1>
-                            {type === 'correct' ? '정답이에요!' : '틀렸어요.'}
-                        </Button1>
-                    </div>
-                </div>
-            ))}
-        </div>
-    );
+  return (
+    <div className={classNames($.scoreButtonContainer)}>
+      <div
+        className={classNames($.scoreButtonItem, {
+          [$[`scoreButtonItemClicked`]]: state,
+          [$[`scoreButtonItemcorrect`]]: true,
+          [$[`textClicked`]]: state,
+        })}
+        onClick={() => handleClick(true)}
+      >
+        <CheckCircle
+          className={classNames($.icon, {
+            [$.iconCorrectClicked]: state,
+          })}
+        />
+        <Button1>정답이에요!</Button1>
+      </div>
+      <div
+        className={classNames($.scoreButtonItem, {
+          [$[`scoreButtonItemClicked`]]: state !== null && !state,
+          [$[`scoreButtonItemincorrect`]]: true,
+          [$[`textClicked`]]: state !== null && !state,
+        })}
+        onClick={() => handleClick(false)}
+      >
+        <XCircle
+          className={classNames($.icon, {
+            [$.iconCorrectClicked]: state,
+          })}
+        />
+        <Button1>틀렸어요!</Button1>
+      </div>
+    </div>
+  );
 };
 
 export default ScoreButton;

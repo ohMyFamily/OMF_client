12081c163bb95c13ab16e5dd4f2ffd0c44d2586e
skrgus6269 <knh6269@gmail.com>
2024-11-14 18:09:48 +0900

    OMF-41 feat: useSuspenseQuery 설정

--- src/apis/queries/question.ts
+++ src/apis/queries/question.ts
@@ -1,15 +1,16 @@
-import { useQuery } from "@tanstack/react-query"  
+import { useSuspenseQuery } from "@tanstack/react-query"  
 
 export const useGetSample  = () => {
     const fetchFunction = async() => {
         const response = await fetch(
             'https://jsonplaceholder.typicode.com/posts'
         ); 
-        return response.json();
+        return await response.json();
     }
     
-    return useQuery({
+    return useSuspenseQuery({
         queryKey: ['repoData'],
-        queryFn: fetchFunction,
+        queryFn: fetchFunction, 
+        staleTime: 1000 * 10, 
     }); 
 };

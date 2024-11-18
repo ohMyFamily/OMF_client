7339bd144d74d4cdee0f01de4d99142bc3bfee6f
daeunleeeee <lde7953@gmail.com>
2024-11-19 03:14:55 +0900

    OMF-42 remove: StrictMode 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from '@/App';
 import './index.scss';
@@ -6,11 +5,9 @@ import { ToastProvider } from './hooks/useToast/context';
 import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
     <ToastProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </ToastProvider>
-  </StrictMode>
 );

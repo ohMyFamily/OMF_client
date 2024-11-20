3d05a40927e771b1bfa1f9aca1ca5ac43fd74b57
skrgus6269 <knh6269@gmail.com>
2024-11-21 03:10:51 +0900

    OMF-46 feat: 임시 네비게이션 페이지 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,36 @@
 import { useNavigate } from 'react-router-dom';
 import $ from '../App.module.scss';
-import ResultCard from '@/components/common/Card/ResultCard';
-import GradingCard from '@/components/common/Card/GradingCard';
-import AnswerListItem from '@/components/common/Card/AnswerListItem';
-import { useEffect, useRef, useState } from 'react';
-import Call from '@/assets/svg/Tossface/Call.svg';
-import AnimateCardSample from '@/components/domain/CheckScore/AnimateCardSample';
+import classNames from 'classnames';
 
 const IndexPage = () => {
   const navigate = useNavigate();
 
-  const [isOpen, setIsOpen] = useState(false);
+  const navigation = [
+    { route: '/login', name: '로그인', state: true },
+    { route: '/main', name: '자식-메인', state: true },
+    { route: '/guide', name: '가이드', state: true },
+    { route: '/splash', name: '스플래시스크린', state: false },
+    { route: '/test', name: '문제풀기', state: true },
+    { route: '/type', name: '부모님 선택', state: true },
+    { route: '/check-score', name: '점수 확인', state: true },
+  ];
 
   return (
     <>
       <div className={$.Wrapper}>
-        <button onClick={() => navigate('/homepage')}>이동</button>
-        <ResultCard score={100} variant='score' />
-        <AnimateCardSample />
-        <GradingCard
-          title={`어머니와\n언제 마지막으로 전화했나요?`}
-          cardImage={Call}
-          cardNumber={1}
-          answer={'정답'}
-        />
-        <AnswerListItem
-          nickname='낙현'
-          score={80}
-          cardImage=''
-          isOpen={isOpen}
-          setIsOpen={setIsOpen}
-        />
+        <div className={$.navigation}>
+          <h1>임시 네비게이션</h1>
+          {navigation.map((item) => {
+            return (
+              <button
+                className={classNames($.navigationButton, { [$.state]: !item.state })}
+                onClick={() => navigate(item.route)}
+              >
+                {item.name}
+              </button>
+            );
+          })}
+        </div>
       </div>
     </>
   );

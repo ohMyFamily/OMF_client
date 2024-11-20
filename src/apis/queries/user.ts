17d234ca77356f651fca03b382c850167e76513a
daeunleeeee <lde7953@gmail.com>
2024-11-21 01:25:16 +0900

    OMF-42 fix: api 타입 파일을 생성하여 타입 별도 관리

--- src/apis/queries/user.ts
+++ src/apis/queries/user.ts
@@ -1,24 +1,7 @@
-import { useMutation, UseMutationResult } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 import { setAuthToken } from '@/apis/instance';
 import { kakaoLogin } from '../api/auth';
-
-interface LoginApiResponseFormat<T> {
-    isSuccess: boolean;
-    code: string;
-    message: string;
-    data: T;
-}
-
-interface KakaoLoginData {
-    accessToken: string;
-    refreshToken: string;
-    grantType: string;
-    expiresIn: number;
-}
-
-export type KakaoLoginResponse = LoginApiResponseFormat<KakaoLoginData>;
-export type useKakaoLoginQuery = UseMutationResult<KakaoLoginResponse, Error, string>;
 
 
 export const useKakaoLoginQuery = () => {

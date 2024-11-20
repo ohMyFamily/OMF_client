4b6ce41a8bbe4ff8f30c40c870b8042afa60a0b3
daeunleeeee <lde7953@gmail.com>
2024-11-21 01:34:00 +0900

    OMF-42 fix: 카카오 로그인 상수를 develop.constants.ts로 이동

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,15 +1,11 @@
 import Button from '@/components/common/Button';
 import KakaoLogo from '@/assets/svg/KakaoLogo.svg'
+import { kakaoLoginLink } from '@/constants/develop.constants';
 
 
 export default function Login() {
-  const REST_API_KEY = import.meta.env.VITE_REST_API_KEY;
-  const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI;
-
-  const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
-
   const handleKakaoLogin =()=>{
-  window.location.href = link;
+    window.location.href = kakaoLoginLink;
   }
 
   return(

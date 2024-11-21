f5a9bf0d38108d632bd6b607160862f46328a717
daeunleeeee <lde7953@gmail.com>
2024-11-22 00:31:37 +0900

    OMF-47 feat: 자식 메인 페이지 스타일 변경

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -20,15 +20,20 @@ export default function Main() {
 
   return (
     <div className={classNames($.Wrapper)}>
-      <img src={MainIllust} alt="메인 일러스트" />
 
-      <div className={classNames($.TextWrapper)}>
-        <Title1>당신의 가족<br/>어디까지 알고 있나요?</Title1>
-        <div className={classNames($.ButtonContainer)}>
-          <Button variant="primary" onClick={handleTest}>테스트 시작하기</Button>
-          <Button variant="tertiary" onClick={handleCheckScore}>내 점수 확인하기</Button>
+      <div className={classNames($.Container)}>
+        <img src={MainIllust} className={classNames($.BackgroundSvg)} alt='메인 일러스트'/>
+
+        <div className={classNames($.TextWrapper)}>
+          <Title1>당신의 가족<br/>어디까지 알고 있나요?</Title1>
+          <div className={classNames($.ButtonContainer)}>
+            <Button variant="primary" onClick={handleTest}>테스트 시작하기</Button>
+            <Button variant="tertiary" onClick={handleCheckScore}>내 점수 확인하기</Button>
+          </div>
+          <div className={classNames($.LogoutButton)}>
+            <Button2 underline={true} onClick={handleLogout}>로그아웃</Button2>
+          </div>
         </div>
-        <Button2 underline={true} onClick={handleLogout}>로그아웃</Button2>
       </div>
     </div>
   );

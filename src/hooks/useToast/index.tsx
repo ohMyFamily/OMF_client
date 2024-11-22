c88be2af59ff9b976aa14d50824505da1dfeb952
daeunleeeee <lde7953@gmail.com>
2024-11-23 05:25:05 +0900

    OMF-48 feat: 로그아웃 시 토스트 호출, 토스트 style props 전달

--- src/hooks/useToast/index.tsx
+++ src/hooks/useToast/index.tsx
@@ -6,25 +6,32 @@ import { useToastContext } from './context';
 export interface ToastType {
   content: string;
   id: string;
+  style?: React.CSSProperties;
 }
 
 export function useToast() {
   const { toasts, setToasts } = useToastContext();
 
-  const addToasts = (content: string) => {
-    setToasts([...toasts, { content, id: Date.now().toString() }]);
+  const addToasts = (content: string, style?: React.CSSProperties) => {
+    setToasts([...toasts, { content, id: Date.now().toString(), style }]);
   };
 
   const deleteToast = (id: string) => {
-    const filterd = toasts.filter((item) => item.id !== id);
-    setToasts(filterd);
+    const filtered = toasts.filter((item) => item.id !== id);
+    setToasts(filtered);
   };
 
   const ToastComponent = () => (
     <Portal>
       <div className={$.toast}>
         {toasts.map((item) => (
-          <Toast content={item.content} key={item.id} id={item.id} deleteToast={deleteToast} />
+          <Toast 
+            content={item.content} 
+            key={item.id} 
+            id={item.id} 
+            style={item.style}
+            deleteToast={deleteToast}
+          />
         ))}
       </div>
     </Portal>
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -2,21 +2,30 @@ import Button from "@/components/common/Button";
 import { Button2, Title1 } from "@/components/common/Typography";
 import $ from './main.module.scss';
 import classNames from 'classnames';
-import { handleLogout } from "@/apis/instance";
 import { useNavigate } from 'react-router-dom';
 import MainIllust from "@/assets/svg/MainIllust.svg";
+import { useToast } from "@/hooks/useToast";
+import { Storage } from "@/storage";
 
 
 export default function Main() {
   const navigate = useNavigate();
+  const { addToasts } = useToast();
 
   const handleTest = () => {
     navigate('/test')
   }
 
   const handleCheckScore = () => {
-    navigate('/check-score')
+    navigate('/check-score');
+    // addToasts('점수 확인 페이지로 이동');
   }
+
+  const handleLogout = () => {
+    Storage.clearItems();
+    addToasts('로그아웃 완료! 다음에 또 만나요~', { bottom: '150px'});
+    navigate('/login');
+  }; 
 
   return (
     <div className={classNames($.Wrapper)}>

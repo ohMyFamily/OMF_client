4824f6f7212b51b59b2f7ef0d9ddd8ac037ff8b2
skrgus6269 <knh6269@gmail.com>
2024-11-23 23:18:46 +0900

    OMF-52 feat: useFunnel를 활용한 채점 로직 구현

--- src/pages/grading/index.tsx
+++ src/pages/grading/index.tsx
@@ -1,3 +1,33 @@
-export default function Main() {
-  return <div>메인 페이지</div>;
+import CheckLayout from '@/container/grading/check';
+import CompleteLayout from '@/container/grading/complete';
+import GuideLayout from '@/container/grading/guide';
+import MainLayout from '@/container/grading/main';
+import { useFunnel } from '@/hooks/useFunnel';
+import { useState } from 'react';
+
+export default function Grading() {
+  const step = ['메인', '가이드', '채점', '완료'];
+  const [answerList, setAnswerList] = useState<(boolean | null)[]>(Array(10).fill(null));
+
+  const funnelData = {
+    answer: [answerList, setAnswerList] as [(boolean | null)[], typeof setAnswerList],
+  };
+  const { FunnelComponent: Funnel, handleStep } = useFunnel(step, funnelData);
+
+  return (
+    <Funnel>
+      <Funnel.Steps name="메인">
+        <MainLayout handleStep={handleStep} />
+      </Funnel.Steps>
+      <Funnel.Steps name="가이드">
+        <GuideLayout handleStep={handleStep} />
+      </Funnel.Steps>
+      <Funnel.Steps name="채점">
+        <CheckLayout handleStep={handleStep} />
+      </Funnel.Steps>
+      <Funnel.Steps name="완료">
+        <CompleteLayout handleStep={handleStep} />
+      </Funnel.Steps>
+    </Funnel>
+  );
 }

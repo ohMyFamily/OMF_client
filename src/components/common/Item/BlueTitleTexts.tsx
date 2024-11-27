cfbd396f0e4cf66156486e6ca75f043ccbf2c0bb
daeunleeeee <lde7953@gmail.com>
2024-11-28 00:32:26 +0900

    OMF-50 fix: login 페이지 관련 파일 container 폴더로 이동

--- src/components/common/Item/BlueTitleTexts.tsx
+++ src/components/common/Item/BlueTitleTexts.tsx
@@ -1,6 +1,6 @@
 import React from 'react'; 
 import classNames from 'classnames';
-import $ from './BlueTitleText.module.scss';
+import $ from './blueTitleText.module.scss';
 
 interface BlueTitleTextProps {
     children: React.ReactNode;

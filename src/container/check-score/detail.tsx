176edf76c50eacb2284e31d9a0f2ddfcbf00a24a
daeunleeeee <lde7953@gmail.com>
2024-11-29 01:29:27 +0900

    OMF-56 feat: 채점 상세 결과 페이지 구현

--- src/container/check-score/detail.tsx
+++ src/container/check-score/detail.tsx
@@ -1,12 +1,72 @@
-import { useParams } from 'react-router-dom';
+import $ from './checkScoreDetail.module.scss';
+import classNames from 'classnames';
+import AppBar from '@/components/common/AppBar';
+import { useNavigate } from 'react-router-dom';
+import { Body1, Title2 } from '@/components/common/Typography';
+import GradingCard from '@/components/common/Card/GradingCard';
+import ResultCard from '@/components/common/Card/ResultCard';
+import AnimateCardSample from '@/components/domain/CheckScore/AnimateCardSample';
+import Button from '@/components/common/Button';
+import KakaoLogo from '@/assets/svg/KakaoLogo.svg';
+import { ResultCardProps } from '@/components/common/Card/ResultCard';
 
-export default function CheckScoreDetail() {
-  const { id } = useParams();
+interface CheckScoreDetailProps extends ResultCardProps {
+  nickname: string;
+  explanation: string;
+}
+
+export default function CheckScoreDetailLayout({
+  score,
+  nickname,
+  image,
+  title,
+  description,
+  explanation,
+}: CheckScoreDetailProps) {
+  const navigate = useNavigate();
+
+  const onBack = () => {
+    navigate(-1);
+  };
+
+  const handleShare = () => {};
+
+  const handleNewTest = () => {
+    navigate('/test');
+  };
 
   return (
-    <div>
-      <h1>점수 상세보기 </h1>
-      <h1>점수id: {id}</h1>
+    <div className={classNames($.Wrapper)}>
+      <AppBar leftRole="back" onClickLeftButton={onBack} />
+      <div className={classNames($.Container)}>
+        <div className={classNames($.ItemContainer)}>
+          <Title2>
+            {nickname}가
+            <br /> 채점한 내 점수는?
+          </Title2>
+          <ResultCard
+            score={score}
+            image={image}
+            title={title}
+            description={description}
+            variant="score"
+          />
+          <Body1>{explanation}</Body1>
+        </div>
+        <div className={classNames($.AnimateCard)}>
+          <Title2>더 많은 카드를 수집해보세요!</Title2>
+          <AnimateCardSample />
+        </div>
+
+        <div className={classNames($.ButtonContainer)}>
+          <Button variant="kakaoLogin" onClick={handleShare} icon={KakaoLogo}>
+            테스트 공유하기
+          </Button>
+          <Button variant="secondary" onClick={handleNewTest}>
+            새로운 테스트 시작하기
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/check-score/detail.tsx
+++ src/pages/check-score/detail.tsx
@@ -1,12 +1,75 @@
-import { useParams } from 'react-router-dom';
+import CheckScoreDetailLayout from '@/container/check-score/detail';
+import { P100, P20, P40, P60, P80, P90 } from '@components/common/TossFace';
+
+const scoreMockData = [
+  {
+    imageKey: P20,
+    nickname: '엄망',
+    title: '말하는 감자',
+    description: '저는 말하는 감자라구요. 아시겠어요?',
+    explanation:
+      '사실 당신은 인간이 된 꿈을 꾸고 있는 감자인 것입니다. 감자가 사람이 되기 위해서는 쑥과 마늘을 먹어야 하지만, 당신은 그러지 않았군요.',
+    score: 20,
+  },
+  {
+    imageKey: P40,
+    nickname: '어무니',
+    title: '효자 맛보기 스푼',
+    description: '효도를 이렇게 콕 찍어서 뇸!',
+    explanation:
+      '효도는 한 스푼씩, 조금씩 실천하는 것부터 시작입니다. 지금부터라도 시작해보는 건 어떨까요?',
+    score: 40,
+  },
+  {
+    imageKey: P60,
+    nickname: '아부지',
+    title: '효자 호소인',
+    description: '어어색할거같진 않고 분명히어색할거같아요',
+    explanation:
+      '어색하지만 그래도 노력하는 모습이 보이네요. 조금 더 자연스러워질 수 있도록 해보아요.',
+    score: 60,
+  },
+  {
+    imageKey: P80,
+    nickname: '아빵',
+    title: '말하는 감자',
+    description: '저는 말하는 감자라구요. 아시겠어요?',
+    explanation: '무럭무럭 자라나는 새싹처럼, 당신의 효심도 쑥쑥 자라나고 있습니다!',
+    score: 80,
+  },
+  {
+    imageKey: P90,
+    nickname: '엄마아',
+    title: '홍진효',
+    description: '홍진효',
+    explanation: '거의 다 왔어요! 조금만 더 노력하면 완벽한 효자/효녀가 될 수 있을 거예요.',
+    score: 90,
+  },
+  {
+    imageKey: P100,
+    nickname: '아빠앙',
+    title: '인간 코지마',
+    description: '사랑하니까 바디프렌드 (둘 다 광고 아님)',
+    explanation: '당신은 이미 완벽한 효자/효녀입니다! 이대로만 계속 해주세요.',
+    score: 100,
+  },
+];
 
 export default function CheckScoreDetail() {
-  const { id } = useParams();
+  //임시
+  const data = scoreMockData[0];
 
   return (
     <div>
-      <h1>점수 상세보기 </h1>
-      <h1>점수id: {id}</h1>
+      <CheckScoreDetailLayout
+        nickname={data.nickname}
+        image={data.imageKey}
+        title={data.title}
+        description={data.description}
+        explanation={data.explanation}
+        variant="score"
+        score={data.score}
+      />
     </div>
   );
 }

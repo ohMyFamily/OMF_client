8a5de0fdb56993196f3799740a179ceaf72dd17e
daeunleeeee <lde7953@gmail.com>
2024-11-29 01:30:34 +0900

    OMF-56 fix: ResultCard props 수정에 따른 AnimateCard 수정

--- src/components/domain/CheckScore/AnimateCardSample/index.tsx
+++ src/components/domain/CheckScore/AnimateCardSample/index.tsx
@@ -1,12 +1,20 @@
 import { useEffect, useRef, useState } from 'react';
 import $ from './animateCardSample.module.scss';
 import ResultCard from '@/components/common/Card/ResultCard';
+import { scoreImages, scoreTable } from '@/constants/business.constants';
 
 export default function AnimateCardSample() {
   const scrollRef = useRef<HTMLDivElement | null>(null);
   const scrollIntervalRef = useRef<number>();
   const scrollSpeed = 15;
   const [isScrollingRight, setIsScrollingRight] = useState(true);
+
+  const cardData = scoreTable.map((data) => ({
+    score: parseInt(data.imageKey.slice(1)),
+    image: scoreImages[data.imageKey as keyof typeof scoreImages],
+    title: data.title,
+    description: data.description,
+  }));
 
   useEffect(() => {
     const scrollContainer = scrollRef.current;
@@ -54,11 +62,17 @@ export default function AnimateCardSample() {
 
   return (
     <div className={$.sample} ref={scrollRef}>
-      <ResultCard score={20} variant='animate' isHas={true} />
-      <ResultCard score={40} variant='animate' isHas={false} />
-      <ResultCard score={60} variant='animate' isHas={false} />
-      <ResultCard score={80} variant='animate' isHas={false} />
-      <ResultCard score={100} variant='animate' isHas={false} />
+      {cardData.map((card) => (
+        <ResultCard
+          key={card.score}
+          score={card.score}
+          variant="animate"
+          isHas={false}
+          image={card.image}
+          title={card.title}
+          description={card.description}
+        />
+      ))}
     </div>
   );
 }

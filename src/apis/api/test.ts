82cfd415e7c3ebf863a1f0a1308596ff36a0da15
daeunleeeee <lde7953@gmail.com>
2024-12-03 01:34:53 +0900

    OMF-50 fix: 애칭 설정 api 요청 헤더 삭제

--- src/apis/api/test.ts
+++ src/apis/api/test.ts
@@ -2,6 +2,7 @@ import { instance } from '../instance';
 import { Storage } from '@/storage';
 import { NicknameData, NicknameResponse } from './test.types';
 
+// 애칭 설정 api
 export const sendNickname = async (nickname: string): Promise<NicknameResponse> => {
   const token = Storage.getItem('accessToken');
   console.log('Access Token:', token);
@@ -9,10 +10,6 @@ export const sendNickname = async (nickname: string): Promise<NicknameResponse> 
   const requestData: NicknameData = {
     nickname: nickname,
   };
-  const response = await instance.post('api/v1/quiz/nickname', requestData, {
-    headers: {
-      Authorization: token,
-    },
-  });
+  const response = await instance.post('api/v1/quiz/nickname', requestData);
   return response.data;
 };

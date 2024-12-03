c73407ecd3cdef635c031a8755fdc01fc1d727cd
daeunleeeee <lde7953@gmail.com>
2024-12-03 17:08:20 +0900

    OMF-82 feat: 문제 풀이 완료 페이지 구현

---
+++ src/container/test/done/index.tsx
@@ -0,0 +1,44 @@
+import { Body1, Title2 } from '@/components/common/Typography';
+import $ from './done.module.scss';
+import classNames from 'classnames';
+import KakaoShareButton from '@/components/common/KakaoShareButton';
+import Button from '@/components/common/Button';
+import { useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import { AnimateClap } from '@/components/common/TossFace';
+
+function TestCompletedLayout() {
+  const navigate = useNavigate();
+  const { nickname } = useParams();
+
+  const handleCopy = () => {};
+
+  const handleRest = () => {
+    navigate('/main');
+  };
+
+  return (
+    <div className={classNames($.Wrapper)}>
+      <img src={AnimateClap} alt="박수 이미지" />
+
+      <Title2>문제를 다 풀었어요!</Title2>
+      <Body1>
+        답안지를 {nickname}에게 보내서
+        <br /> 채점을 받아볼까요?
+      </Body1>
+
+      <div className={classNames($.ButtonContainer)}>
+        <KakaoShareButton />
+        <Button variant="secondary" onClick={handleCopy}>
+          링크 복사
+        </Button>
+      </div>
+
+      <Button variant="primary" onClick={handleRest}>
+        처음으로 돌아가기
+      </Button>
+    </div>
+  );
+}
+
+export default TestCompletedLayout;

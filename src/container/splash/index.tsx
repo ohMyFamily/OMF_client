cb5bcdf89d79b01105c4fcdf88eb0910bdc4dc9e
daeunleeeee <lde7953@gmail.com>
2024-12-11 16:58:20 +0900

    OMF-88 fix: 스플래시 프레임 누락 수정

--- src/container/splash/index.tsx
+++ src/container/splash/index.tsx
@@ -1,15 +1,17 @@
 import { useEffect, useState } from 'react';
 import splash1 from '@/assets/image/splash1.png';
 import splash2 from '@/assets/image/splash2.png';
+import splash3 from '@/assets/image/splash3.png';
+import splash4 from '@/assets/image/splash4.png';
 import $ from './splash.module.scss';
 
 export default function SplashLayout() {
   const [imageIndex, setImageIndex] = useState(0);
-  const images = [splash1, splash2];
+  const images = [splash1, splash2, splash3, splash4];
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setImageIndex((prev) => (prev + 1) % 2);
+      setImageIndex((prev) => (prev + 1) % 4);
     }, 1000);
 
     return () => clearInterval(interval);

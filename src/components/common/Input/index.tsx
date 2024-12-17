c4581110b70325f7ce5addb74ff440925bf13610
daeunleeeee <lde7953@gmail.com>
2024-12-17 22:47:41 +0900

    OMF-82 feat: 장문 입력창 구현

--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -2,6 +2,7 @@ import classNames from 'classnames';
 import { ChangeEvent, useEffect, useState, useRef, SetStateAction, Dispatch } from 'react';
 import $ from '@/components/common/Input/Input.module.scss';
 import X from '@/assets/svg/X.svg?react';
+
 interface InputProps {
   text: string;
   setText: Dispatch<SetStateAction<string>>;
@@ -10,22 +11,37 @@ interface InputProps {
 
 export default function Input({ text, setText, maxLength }: InputProps) {
   const [isTyping, setIsTyping] = useState<boolean>(false);
-  //input을 focus하기 위해 useRef 사용(handleClear 실행 이후에도 포커스가 유지되도록)
-  const inputRef = useRef<HTMLInputElement>(null);
+  //textarea을 focus하기 위해 useRef 사용(handleClear 실행 이후에도 포커스가 유지되도록)
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
   //div 밖 영역 감지를 위해
   const inputWrapperRef = useRef<HTMLDivElement>(null);
 
-  const onChangeText = (e: ChangeEvent<HTMLInputElement>) => {
+
+  // 장문 입력 시 textarea 높이를 자동으로 조절함 
+  const handleResizeHeight = () => {
+    const textarea = textareaRef.current;
+    if (textarea) {
+      textarea.style.height = 'auto';
+      const newHeight = textarea.scrollHeight; 
+      textarea.style.height = `${newHeight}px`; 
+    }
+  };
+
+  // 입력이 변경될 때마다 높이 조절
+  useEffect(() => {
+    handleResizeHeight();
+  }, [text]);
+
+  const onChangeText = (e: ChangeEvent<HTMLTextAreaElement>) => {
     const newValue = e.target.value;
     if (!maxLength || newValue.length <= maxLength) {
-      // maxLength가 있을 때만 검사
       setText(newValue);
     }
   };
 
   const handleClear = () => {
     setText('');
-    inputRef.current?.focus();
+    textareaRef.current?.focus();
   };
 
   useEffect(() => {
@@ -36,12 +52,11 @@ export default function Input({ text, setText, maxLength }: InputProps) {
     }
   }, [text]);
 
-  //input div 밖의 영역 터치 시 istyping = false, input 포커스 해제
   useEffect(() => {
     const handleInputDivOutside = (event: MouseEvent | TouchEvent) => {
       if (inputWrapperRef.current && !inputWrapperRef.current.contains(event.target as Node)) {
         setIsTyping(false);
-        inputRef.current?.blur();
+        textareaRef.current?.blur();
       }
     };
 
@@ -56,10 +71,11 @@ export default function Input({ text, setText, maxLength }: InputProps) {
 
   return (
     <div ref={inputWrapperRef} className={classNames($.inputWrapper)}>
-      <input
-        ref={inputRef}
-        value={text}
+      <textarea
         className={classNames($.input)}
+        rows={1}
+        ref={textareaRef}
+        value={text}
         maxLength={maxLength}
         onChange={onChangeText}
       />
